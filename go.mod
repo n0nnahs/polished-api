12477c862f3be6993f925890c25e8c025e421985
Shannon Woods <45649247+n0nnahs@users.noreply.github.com>
2022-05-22 00:07:42 -0400

    added route

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/n0nnahs/polished-api
 go 1.16
 
 require (
+	github.com/go-playground/validator/v10 v10.11.0 // indirect
 	github.com/gorilla/mux v1.8.0
 	github.com/joho/godotenv v1.4.0 // indirect
 	go.mongodb.org/mongo-driver v1.9.1
--- routes/routes.go
+++ routes/routes.go
@@ -1,7 +1,10 @@
 package routes
 
-import "github.com/gorilla/mux"
+import (
+	"github.com/gorilla/mux"
+	"github.com/n0nnahs/polished-api/controllers"
+)
 
 func PolishRoute(router *mux.Router){
-
+    router.HandleFunc("/user", controllers.CreatePolish()).Methods("POST")
 }
