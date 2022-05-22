aa757a50672932c11c98fa5117e57cf96a774166
Shannon Woods <45649247+n0nnahs@users.noreply.github.com>
2022-05-21 23:42:16 -0400

    updated model

---
+++ models/polish_model.go
@@ -0,0 +1,18 @@
+package polish
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Polish struct {
+	ID               primitive.ObjectID `bson:"_id" json:"id"`
+	Name             string        	 `bson:"name" json:"name" validate:"required"`
+	Brand            string        	 `bson:"brand" json:"brand" validate:"required"`
+	Collection       string       	 `bson:"collection" json:"collection,omitempty"`
+	Type             []string    		 `bson:"type" json:"type,omitempty"`
+	Color            []string     	 `bson:"color" json:"color,omitempty"`
+	ColorDescription []string     	 `bson:"color_description" json:"colorDescription,omitempty"`
+	PurchaseDate     string       	 `bson:"purchase_date" json:"purchaseDate,omitempty"`
+	Description      string       	 `bson:"description" json:"description,omitempty"`
+	Swatched         string       	 `bson:"swatched" json:"swatched" validate:"required"`
+}
